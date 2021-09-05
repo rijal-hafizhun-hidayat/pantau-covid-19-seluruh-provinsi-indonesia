844a7f68bfbd676699c8ff7b69b375a8703e4c51
Rijal Hafizhun Hidayat <62733019+rijal-hafizhun-hidayat@users.noreply.github.com>
2021-09-05 07:08:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -99,6 +99,37 @@ if (isset($_POST["masuk"])) {
           </div>
         </div>
       </div>
+      <h3>kasus corona virus global</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">Negara</th>
+            <th scope="col">Positif</th>
+            <th scope="col">Sembuh</th>
+            <th scope="col">Meninggal</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 
