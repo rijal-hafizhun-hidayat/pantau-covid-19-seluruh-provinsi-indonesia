670b16c5c9262fff31b63bd919881f043c05502c
Rijal Hafizhun Hidayat <62733019+rijal-hafizhun-hidayat@users.noreply.github.com>
2021-09-14 19:06:19 +0700

    adding class hospital and some other function

--- logic/proses.php
+++ logic/proses.php
@@ -27,7 +27,8 @@ class virus
     {
         return $this->covid[$this->daerah]["attributes"]["Kasus_Meni"];
     }
-    public function print_country($counter){
+    public function print_country($counter)
+    {
         return $this->covid[$counter]["attributes"]["Country_Region"];
     }
     public function print_positive($counter)
@@ -44,7 +45,44 @@ class virus
     }
 }
 
+class rumah_sakit{
+    public $provinsi;
+    public $kota;
+    public $hospital;
+    public $hospital_parsing;
+    public function __construct($province, $city, $json){
+        $this->provinsi = $province;
+        $this->kota = $city;
+        $this->hospital = file_get_contents($json);
+        $this->hospital_parsing = json_decode($this->hospital, true);
+
+    }
+    public function print_hospital_name($count){
+        return $this->hospital_parsing["hospitals"][$count]["name"];
+    }
+    public function looping(){
+        $loop = $this->hospital_parsing["hospitals"];
+        $ulang = sizeof($loop);
+        return $ulang;
+    }
+    public function print_hospital_address($count){
+        return $this->hospital_parsing["hospitals"][$count]["address"];
+    }
+    public function print_hospital_available($count){
+        return $this->hospital_parsing["hospitals"][$count]["bed_availability"];
+    }
+    public function print_hospital_phone($count){
+        return $this->hospital_parsing["hospitals"][$count]["phone"];
+    }
+    public function print_hospital_update($count){
+        return $this->hospital_parsing["hospitals"][$count]["info"];
+    }
+}
+
+
 $indo = new virus($_POST['daerah'], 'https://api.kawalcorona.com/indonesia/provinsi/');
 $global = new virus(0, 'https://api.kawalcorona.com/');
+
+$hospital = new rumah_sakit($_POST['prov'], $_POST['gedung'], 'https://rs-bed-covid-api.vercel.app/api/get-hospitals?provinceid='. $_POST['prov'] .'&cityid='. $_POST['gedung'] .'&type=1');
 
 ?>
